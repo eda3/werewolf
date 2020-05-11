6c0e9ab68acf8b4cfaa82ce652027356ea26f580
eda3 <gerorin1010@gmail.com>
2020-05-11 20:05:01 +0900

    style: mypy

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,20 @@
 import os
 
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, Context
 
 
 bot: Bot = Bot(command_prefix=";")
 print(f"{type(bot)=}")
 
-def main():
+
+def main() -> None:
     token = os.environ["DISCORD_BOT_TOKEN"]
-    print(f"{type(token)=}")
-    print(token)
     bot.run(token)
 
 
 @bot.command()
-async def ping(ctx):
+async def ping(ctx: Context) -> None:
+    print(f"{type(ctx)=}")
     await ctx.send("pong")
 
 
