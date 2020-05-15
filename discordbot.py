f132bd542f3b9d438460f9a1d5d4617fb217f0d4
eda3 <gerorin1010@gmail.com>
2020-05-16 07:42:03 +0900

    feat: logging

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,18 @@
 import os
+from logging import DEBUG, INFO, Logger
 
 from discord.ext.commands import Bot, Context, command
 
+from setup_logger import setup_logger
+
 
 def main() -> None:
+    logger: Logger = setup_logger(__name__)
+    logger.info("START")
+
     # 接頭辞を「;」に設定
     bot: Bot = Bot(command_prefix=";")
+    logger.debug(bot)
 
     # 環境変数DISCORD_BOT_TOKENからBOTのトークンを取得
     token = os.environ["DISCORD_BOT_TOKEN"]
