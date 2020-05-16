84825c4307cf7d910a199b2e28e809de2fd05806
eda3 <gerorin1010@gmail.com>
2020-05-16 14:40:05 +0900

    style: black

--- cogs/game_status_cog.py
+++ cogs/game_status_cog.py
@@ -4,6 +4,7 @@ from cogs.utils.player import Player
 
 
 from setup_logger import setup_logger
+
 logger = setup_logger(__name__)
 
 
@@ -16,7 +17,6 @@ class GameStatusCog(Cog):
     async def show_game_status(self, ctx: context) -> None:
         await ctx.send("show_game_statusコマンドが実行されました")
         await ctx.send(f"現在のゲームのステータスは{self.bot.game.status}です")
-        # TODO: ゲームステータスを表示する
 
 
 def setup(bot: Bot) -> None:
