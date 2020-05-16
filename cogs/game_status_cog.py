422c4e17b239a3e9e7e1e6927a3af05a2035a3ee
eda3 <gerorin1010@gmail.com>
2020-05-16 18:36:29 +0900

    feat: set_game_status()をぷち実装

--- cogs/game_status_cog.py
+++ cogs/game_status_cog.py
@@ -1,6 +1,6 @@
 from discord.ext.commands import Bot, Cog, command, context
 
-from cogs.utils.player import Player
+from cogs.utils.const import GameStatus
 
 
 from setup_logger import setup_logger
@@ -18,6 +18,26 @@ class GameStatusCog(Cog):
         await ctx.send("show_game_statusコマンドが実行されました")
         status: str = self.bot.game.status.value
         await ctx.send(f"現在のゲームのステータスは{status}です")
+
+    @command(aliases=["setgs"])
+    async def set_game_status(self, ctx: context, status: str = "") -> None:
+        status_list = [x.value for x in GameStatus]
+
+        if status == "":
+            await ctx.send(
+                "引数がありません。引数は以下からえらんでください。"
+                f"{status_list}"
+            )
+            return
+
+        if status not in status_list:
+            await ctx.send(
+                "引数が間違っています。引数は以下からえらんでください。"
+                f"{status_list}"
+            )
+            return
+
+        await ctx.send(f"あなたが選んだ引数は{status}です")
 
 
 def setup(bot: Bot) -> None:
