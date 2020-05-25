e434743a13e3bb941188148d1302306c70419bbd
eda3 <gerorin1010@gmail.com>
2020-05-26 05:29:44 +0900

    tests: メソッド名が間違っていたので修正

--- tests/test_game_status_cog.py
+++ tests/test_game_status_cog.py
@@ -6,23 +6,7 @@ from cogs.utils.roles import Villager, FortuneTeller, Werewolf, Thief
 from cogs.game_status_cog import GameStatusCog
 
 
-@pytest.mark.asyncio
-async def test_is_vote_count_same_for_all_test001():
-    """正常系  投票数が全員 1 の場合"""
-    p1 = Player(1, "P1")
-    p1.after_game_role = Villager
-    p1.vote_count = 1
-
-    p2 = Player(2, "P2")
-    p2.after_game_role = Villager
-    p2.vote_count = 1
-
-    p_list = [p1, p2]
-    assert await GameStatusCog.is_vote_count_same_for_all(p_list) is True
-
-
-@pytest.mark.asyncio
-async def test_is_vote_count_same_for_all_test002():
+async def test_is_vote_count_same_for_all():
     """正常系  投票数が異なる場合"""
     p1 = Player(1, "P1")
     p1.after_game_role = Villager
@@ -41,4 +25,3 @@ async def test_is_vote_count_same_for_all_test002():
 
     # 投票数が異なる場合はFalse
     assert await GameStatusCog.is_vote_count_same_for_all(p_list) is False
-
