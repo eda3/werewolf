80d1d730cb7459def5448c4622b25ec31003870a
eda3 <gerorin1010@gmail.com>
2020-06-03 05:08:13 +0900

    refactor: self.bot.game.player_list -> self.player.list

--- src/cogs/utils/gamerole.py
+++ src/cogs/utils/gamerole.py
@@ -1,36 +1,36 @@
 from typing import List
 
+from discord.ext.commands import Context
 from discord import Emoji, Member, Message, Reaction, utils
 from discord.channel import TextChannel
 
 from cogs.utils.const import SideConst, emoji_list
-from cogs.utils.werewolf_bot import WerewolfBot
 
 
 class GameRole:
     """各役職のスーパークラス"""
 
-    def __init__(self, bot: WerewolfBot) -> None:
-        self.bot: WerewolfBot
+    def __init__(self, player_list) -> None:
+        self.player_list = player_list
         self.name = ""
         self.side = SideConst.WHITE
 
-    async def vote(self, player, channel: TextChannel) -> int:
+    async def vote(self, ctx: Context, player, channel: TextChannel) -> int:
         await channel.send("人狼だと思う人を一人選択してください")
 
-        choice_player = await self.select_player(player, channel)
+        choice_player = await self.select_player(ctx, player, channel)
 
         name = choice_player.name
         await channel.send(f"あなたは{name}に投票しました")
 
         return 1
 
-    async def select_player(self, player, channel: TextChannel):
+    async def select_player(self, ctx: Context, player, channel: TextChannel):
         # 対象を選択
-        p_list = self.bot.game.player_list
+        p_list = self.player_list
         text: str = ""
         choice_emoji: List[Emoji] = []
-        for emoji, p in zip(emoji_list, self.bot.game.player_list):
+        for emoji, p in zip(emoji_list, self.player_list):
             choice_emoji.append(emoji)
             text += f"{emoji} {p.name}"
         await channel.send(text)
@@ -41,10 +41,10 @@ class GameRole:
             await last_message.add_reaction(emoji)
 
         def my_check(reaction: Reaction, user: Member) -> bool:
-            member = utils.get(self.bot.get_all_members(), id=player.id)
+            member = utils.get(ctx.bot.get_all_members(), id=player.id)
             return user == member and str(reaction.emoji) in choice_emoji
 
-        react_emoji, react_user = await self.bot.wait_for(
+        react_emoji, react_user = await ctx.bot.wait_for(
             "reaction_add", check=my_check
         )
         await channel.send(f"{react_user.name}が {react_emoji.emoji} を押したのを確認しました")
